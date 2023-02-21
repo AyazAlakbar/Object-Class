f0c88da7e88d0a65d601cc0c0e92359826b3d998
alexberg90 <ayazya@code.edu.az>
2023-02-21 04:43:18 +0400

    new uptade

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
-alert("salam")
+//alert("salam")
+
+
+function addNumMonth(arr) {
+    return arr.map((month,index)=>(index+1)+ "-"+month);
+}
+
+
+let month=["yanvar","fevral","mart","aprel"]
+let numberedMonth=addNumMonth(month);
+
+
+
+
+console.log(numberedMonth);
+
+
