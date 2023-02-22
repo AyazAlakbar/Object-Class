af84e83c3941a34f4e9b78fc4ed0c6d0051f1406
alexberg90 <ayazya@code.edu.az>
2023-02-22 20:36:58 +0400

    new update

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,3 +34,55 @@ function countWord(string) {
 
 //Task 4
     
+
+class array {
+
+    constructor(){
+        let arr = Object.create(array.prototype)
+        //
+        Object.defineProperty(arr,'length',{
+            value:0,
+            enumerable:false,
+            writable:true
+        })
+        
+        
+        for (let i in arguments) {
+            arr[i] = arguments[i]
+            arr.length += 1
+        }
+
+        return arr
+    }
+
+
+    Push(element){
+        this[this.length] = element
+        this.length++
+        return this.length
+    }
+    
+    Find(cb){
+        let result = new array()
+
+        for (let i in this) {
+
+            if (this.hasOwnProperty(i)) {
+
+                if (cb(this[i])) {
+                    return this[i]
+                }
+            }
+        }
+    }
+}
+
+
+
+let arr = new array(1,2,3,4,5)
+arr.Push(1);
+arr.Push(2);
+
+
+console.log(arr.Find(x=>x>3));
+
