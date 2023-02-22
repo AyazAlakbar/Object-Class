49eae04e7295d742fcc8e7629e5cc96b3cbf4d4b
alexberg90 <ayazya@code.edu.az>
2023-02-22 13:18:51 +0400

    new update

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,9 +9,23 @@ function addNumMonth(arr) {
 let month=["yanvar","fevral","mart","aprel"]
 let numberedMonth=addNumMonth(month);
 
-
-
-
 console.log(numberedMonth);
 
 
+
+
+
+
+
+function countWord(string) {
+    
+    let words = string.split(" ");
+    return string = words.map((word) => word.length);
+    
+  }
+
+
+  let string = "Code Academy Web Development Group P515";
+  let result = countWord(string);
+  console.log(result);
+    
